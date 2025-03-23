fbb76a1699566428fe8541628ed869f0cb1c2ce1
yarendonmez <yarendonmez034@gmail.com>
2025-03-23 21:25:39 +0300

    Polymorphism_Demo

--- src/main/java/com/yarendonmez/_13_polymorphismDemo/CustomerManager.java
+++ src/main/java/com/yarendonmez/_13_polymorphismDemo/CustomerManager.java
@@ -1,14 +1,14 @@
 package com.yarendonmez._13_polymorphismDemo;
 
 public class CustomerManager {
-    private BaseLogger custLogger;
+    private BaseLogger logger;
     public CustomerManager(BaseLogger logger){
-        this.custLogger = logger; // this.logger(private Baselogger logger daki) = logger (public CustomerManager(Baselogger logger)daki
+        this.logger = logger; // this.logger(private Baselogger logger daki) = logger (public CustomerManager(Baselogger logger)daki
     }
 
     public void add(){
         System.out.println("Müşteri eklendi");
-        this.custLogger.log("Log mesajı");
+        this.logger.log("Log mesajı");
 
 
 
